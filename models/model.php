4d372cc0da414aa3bb1a87fbd3aaa9fa9eb73419
Ismail <ismailalri@gmail.com>
2023-02-21 11:04:11 +0100

    feat : save.php, get.php to insert a message and get the list of all messages

--- models/model.php
+++ models/model.php
@@ -2,6 +2,7 @@
 
 class model
 {
+    // table name
     private $table;
     private $db = "id20339000_chatfi";
     private $user = "id20339000_fabisma";
@@ -24,6 +25,22 @@ class model
     public function select()
     {
         $sql = "SELECT * FROM $this->table";
+        return $this->pdo->query($sql)->fetchAll();
+    }
+
+    // insert a message
+    public function insert($data)
+    {
+        $sql = "INSERT INTO $this->table (author, content) VALUES (:author, :content)";
+        $stmt = $this->pdo->prepare($sql);
+        $res = $stmt->execute($data);
+        return $res;
+    }
+
+    // get n latest messages
+    public function selectNLatest($n)
+    {
+        $sql = "SELECT * FROM $this->table ORDER BY id DESC LIMIT $n";
         return $this->pdo->query($sql)->fetchAll();
     }
 
---
+++ models/save.php
@@ -0,0 +1,22 @@
+<?php
+/*Développer un script PHP enregistrer.php permettant l’enregistrement d’un message dans la table
+chat.
+Cette page sera invoquée par des requêtes HTTP GET, dans lesquelles au minimum l’auteur ainsi que
+le contenu du message seront mentionnés. Par exemple : enregistrer.php?pseudo=toto&phrase=salut
+L’estampille horaire devra être fournie par le script PHP, alors que l’identifiant du message sera généré
+automatiquement par MySQL.*/
+include_once("model.php");
+$model = new model("chat");
+$author = $_GET["author"];
+$content = $_GET["content"];
+$data = array(
+    "author" => $author,
+    "content" => $content
+);
+$res = $model->insert($data);
+if ($res) {
+    echo "Message enregistré";
+} else {
+    echo "Erreur";
+}
+?>
